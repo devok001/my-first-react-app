21f227eb668e418831a68e2e353ae422d752b7d2
devok001 <omkar.kshirsagar@jadeglobal.com>
2024-11-14 17:29:10 +0530

    thrird commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import Box from "./components/Box";
 import Board from "./components/Board";
+import { useState } from "react";
+
 function App() {
+
+  
+  
   return <div>
    <Board />
   </div>
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -4,21 +4,25 @@ import Box from "./Box"
 
 function Board(){
    return <>
+
     <div>
        <Box value="" />
        <Box value="" />
        <Box value="" />
     </div>
+
     <div>
        <Box value="" />
        <Box value="" />
        <Box value="" />
     </div>
+
     <div>
        <Box value="" />
        <Box value="" />
        <Box value="" />
     </div>
+
    </>
 }
 
